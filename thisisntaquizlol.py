173e895120705361c7ef15690ead6a7fda739a94
Helloworld20-web <cadion.estrella68@gmail.com>
2025-08-04 14:09:39 +0400

    Add files via upload

---
+++ thisisntaquizlol.py
@@ -0,0 +1,15 @@
+print("This isn't a quiz right?")
+
+a = input("y/n: ")
+
+if (a == "y"):
+    print("\nOk")
+
+elif (a == "n"):
+    print("\nWHAT")
+
+elif (a == "mi"):
+    print("\nIt will come")
+
+else:
+    print("\n:o ?")
